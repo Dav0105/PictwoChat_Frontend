0e88c664a6e224e07c3f7ed6655a048a420dcac8
Dav0105 <120047730+Dav0105@users.noreply.github.com>
2026-02-27 22:18:58 +0100

    Added DrawBox component

---
+++ src/components/DrawBox.tsx
@@ -0,0 +1,134 @@
+import { ReactSketchCanvas, type ReactSketchCanvasRef} from "react-sketch-canvas";
+import { Box, Button, Icon } from '@mui/material'
+import { useRef, useState, type CSSProperties } from "react";
+import ModeIcon from '@mui/icons-material/Mode';
+import ClearIcon from '@mui/icons-material/Clear';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+type DrawBoxProps = {
+    width: string,
+    height: string
+    color?: string
+    displaySaveBtn?: boolean
+}
+
+function DrawBox({width = "500px", height = "450px", color = '#000000', displaySaveBtn = false}: DrawBoxProps) {
+    const canvasRef = useRef<ReactSketchCanvasRef>(null);
+    const [eraseMode, setEraseMode] = useState(false);
+
+    const pressedStyle: CSSProperties = {boxShadow: '1px 1px 0px black', transform: 'translate(2px, 2px)'}
+
+    const pencilPressed = () => {
+        setEraseMode(false)
+        canvasRef.current?.eraseMode(false)
+    }
+
+    const eraserPressed = () => {
+        setEraseMode(true)
+        canvasRef.current?.eraseMode(true)
+    }
+
+    const cleanPressed = () => {
+        canvasRef.current?.clearCanvas()
+    }
+
+    const savedPressed = async () => {
+        let base64data = await canvasRef.current?.exportImage("png")
+        console.log(base64data)
+        if (base64data) {
+            // Process base64data
+            const byteString = atob(base64data.split(',')[1]);
+            const mimeString = base64data.split(',')[0].split(':')[1].split(';')[0];
+            const ab = new ArrayBuffer(byteString.length);
+            const ia = new Uint8Array(ab);
+            for (let i = 0; i < byteString.length; i++) {
+                ia[i] = byteString.charCodeAt(i);
+            }
+            // Create blob
+            const blob = new Blob([ab], { type: mimeString });
+
+            // Create download link
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = "masterpiece.png";
+            document.body.appendChild(link);
+            link.click();
+
+            // Cleanup
+            document.body.removeChild(link);
+            URL.revokeObjectURL(url);
+        }
+
+    }
+
+    let drawBtn;
+    let eraseBtn;
+    if (!eraseMode) {
+        drawBtn = (
+            <Button onClick={pencilPressed} disabled sx={pressedStyle}>
+                <ModeIcon />
+            </Button>
+        )
+        eraseBtn = (
+            <Button onClick={eraserPressed}>
+                <ClearIcon />
+            </Button>
+        )
+    } else {
+        drawBtn = (
+            <Button onClick={pencilPressed}>
+                <ModeIcon />
+            </Button>
+        )
+        eraseBtn = (
+            <Button onClick={eraserPressed} disabled sx={pressedStyle}>
+                <ClearIcon />
+            </Button>
+        )
+    }
+
+    return (
+        <Box sx={{ display: 'grid', justifyContent: 'center', gap: '10px' }}>
+            {/* Buttons */}
+            <Box sx={{ display: 'inline-flex', justifyContent: 'space-between', gap: '2px' }}>
+                <Box sx={{ display: 'inline-flex', justifyContent: 'left', gap: '2px' }}>
+                    {drawBtn}
+                    {eraseBtn}
+                </Box>
+                <Box sx={{ display: 'inline-flex', justifyContent: 'right', gap: '2px' }}>
+                    { displaySaveBtn ? (
+                        <Button onClick={savedPressed}>
+                            Save
+                        </Button>
+                        ) : null
+                    }
+
+                    <Button onClick={cleanPressed}>
+                        <DeleteIcon />
+                    </Button>
+                </Box>
+            </Box>
+            {/* Canvas */}
+            <Box sx={{
+                width: width, 
+                height: height,
+                backgroundColor: 'white',
+                border: '2px solid #1a1a1a',
+                borderRadius: '8px 6px 10px 7px / 7px 10px 6px 9px',
+                boxShadow: '3px 3px 0px #1a1a1a',
+                mx: 'auto',
+            }} >
+                <ReactSketchCanvas
+                    ref={canvasRef}
+                    // width="750px"
+                    // height="450px"
+                    canvasColor="#00000000"
+                    strokeColor={color}   
+                />
+            </Box>
+        </Box>
+    )
+}
+
+export default DrawBox
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Container } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { letterColors } from '../theme'
+import DrawBox from '../components/DrawBox'
 
 const title = "PictwoChat"
 
@@ -32,7 +33,7 @@ function Home() {
               </Box>
             ))}
           </Box>
-          <Box sx={{
+          {/* <Box sx={{
             width: '600px',
             height: '400px',
             backgroundColor: 'white',
@@ -40,7 +41,8 @@ function Home() {
             borderRadius: '8px 6px 10px 7px / 7px 10px 6px 9px',
             boxShadow: '3px 3px 0px #1a1a1a',
             mx: 'auto',
-          }} />
+          }} /> */}
+          <DrawBox width={"600px"} height={"400px"}/>
 
           <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: { xs: '100%', sm: '400px' }, mx: 'auto', mt: '30px' }}>
             <Button variant="contained" sx={{ width: '100%' }}>
