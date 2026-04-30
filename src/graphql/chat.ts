421d767e9b1ac3de3e94568a4ad0390a8a6d6283
relka-dev <relka.dev@gmail.com>
2026-04-30 16:09:21 +0200

    code: started graphql for messages

---
+++ src/graphql/chat.ts
@@ -0,0 +1,60 @@
+import { gql } from "@apollo/client";
+
+export type GetRoomsResponse = {
+  rooms: string[];
+  nb_users: number;
+};
+
+export type RoomVariables = {
+  room: string;
+  nb_users: number;
+};
+
+export type MessageVariables = {
+  username: string;
+  room: string;
+  timestamp: string;
+  text?: string;
+  image?: string; // base64 image
+};
+
+export const sendMessage = gql`
+  mutation SendMessage(
+    $username: String!
+    $room: String!
+    $timestamp: String!
+    $text: String
+    $image: String
+  ) {
+    sendMessage(
+      username: $username
+      room: $room
+      timestamp: $timestamp
+      text: $text
+      image: $image
+    ) {
+      success
+    }
+  }
+`;
+
+export const getMessages = gql`
+  query GetMessages($room: String!) {
+    messages(room: $room) {
+      username
+      room
+      timestamp
+      text
+      image
+    }
+  }
+`;
+
+export const getRooms = gql`
+  query GetRooms {
+    rooms {
+      name
+      nb_users
+    }
+  }
+`;
